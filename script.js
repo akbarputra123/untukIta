3f45388c7dbdac8c5c1eee57bb8943d60ea40e26
akbarputra123 <barltzyml@gmail.com>
2024-09-05 23:12:40 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('forgiveButton').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'flex';
+});
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+});
